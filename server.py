cb09fb92b94d889a9080ca03d1d181679160e1ca
Juho Friman <juho.t.friman@gmail.com>
2024-03-15 17:57:50 +0200

    Minimal endpoint

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from typing import Annotated
 
 app = FastAPI()
 
@@ -6,3 +8,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+app = FastAPI()
+
+
+@app.post("/text", response_class=HTMLResponse)
+async def new_text(text: Annotated[str, Form()]):
+    print(text)
+    return """
+    <div>
+        Some response fragment
+    </div>
+    """
