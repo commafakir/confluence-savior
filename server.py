9e92b84a64e275fe624339f3480c519483163b2b
Juho Friman <juho.t.friman@gmail.com>
2024-03-15 19:03:42 +0200

    Amazing document explorer

--- server.py
+++ server.py
@@ -2,12 +2,29 @@ from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
+import os
 
 app = FastAPI()
 
 @app.get("/hello")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/confluence", response_class=HTMLResponse)
+async def docs():
+    files = os.listdir("./docs")
+    tags = map(lambda a: f"<li hx-target=\"#opened-doc\" hx-get=\"/document/{a}\">{a}</li>", files)
+    return f"""
+        <ul>{"".join(tags)}</ul>
+    """
+@app.get("/document/{doc}", response_class=HTMLResponse)
+async def docs(doc):
+    with open(f"./docs/{doc}", "r") as file:
+        data = file.read()
+    return f"""
+        <div>{data}</div>
+    """
+
 
 @app.post("/text", response_class=HTMLResponse)
 async def new_text(text: Annotated[str, Form()]):
